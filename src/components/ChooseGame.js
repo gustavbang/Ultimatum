e00f948480502141ed76fe24f36f6f29911045a7
Gustav Bang <gustavbang96@gmail.com>
2019-11-18 19:54:32 +0100

    Categories

--- src/components/ChooseGame.js
+++ src/components/ChooseGame.js
@@ -2,6 +2,10 @@ import React from 'react'
 import {Grid} from '@material-ui/core'
 import Background from "./Reuseables/Background";
 
+import WarmUp from '../assets/img/categories/warmup.png'
+import BoysNight from '../assets/img/categories/boysnight.png'
+import GirlsNight from '../assets/img/categories/girlsnight.png'
+import Halloween from '../assets/img/categories/halloween.png'
 
 
 
@@ -12,6 +16,13 @@ class ChooseGame extends React.Component {
         return (
             <Grid container style={{width: '100%'}}>
                 <Background/>
+
+                <img src={WarmUp} style={{width: '80vw', marginLeft: '10vw', marginTop: '20px'}}/>
+                <img src={BoysNight} style={{width: '80vw', marginLeft: '10vw', marginTop: '10px'}}/>
+                <img src={GirlsNight} style={{width: '80vw', marginLeft: '10vw', marginTop: '10px'}}/>
+                <img src={Halloween} style={{width: '80vw', marginLeft: '10vw', marginTop: '10px'}}/>
+
+
             </Grid>
         )
     }
